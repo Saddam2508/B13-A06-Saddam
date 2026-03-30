c9fdf695621ecea1af4345e211daa8bd857927e0
Saddam <eyarinsultana@gmail.com>
2026-03-30 08:22:08 +0600

    navbar completed

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,86 @@
+import { ShoppingCart } from "lucide-react";
+import React from "react";
+import CartIconWithBadge from "../ui/CartIconWithBadge";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100 shadow-sm max-w-350 mx-auto">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <a>Products</a>
+            </li>
+
+            <li>
+              <a>Features</a>
+            </li>
+            <li>
+              <a>Pricing</a>
+            </li>
+            <li>
+              <a>Testimonials</a>
+            </li>
+            <li>
+              <a>FAQ</a>
+            </li>
+          </ul>
+        </div>
+        <a className="text-2xl font-bold bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-transparent bg-clip-text">
+          DigiTools
+        </a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a>Products</a>
+          </li>
+          <li>
+            <a>Features</a>
+          </li>
+          <li>
+            <a>Pricing</a>
+          </li>
+          <li>
+            <a>Testimonials</a>
+          </li>
+          <li>
+            <a>FAQ</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end flex gap-4">
+        <CartIconWithBadge
+          itemCount={50}
+          onClick={() => console.log("hello")}
+        />
+        <a className="btn ">Login</a>
+        <a className="btn bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-2xl text-white border-none">
+          Get Started
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
