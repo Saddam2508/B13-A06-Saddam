9da52370f07c3fc605a35c9bae27b83fe58cc3fc
Saddam <eyarinsultana@gmail.com>
2026-03-31 08:57:05 +0600

    catd completed

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,60 @@
 import React from "react";
+import { ShoppingCart } from "lucide-react";
 
-const Cart = () => {
-  return <div>cart</div>;
+const Cart = ({ cart, setCart }) => {
+  const total = cart.reduce((acc, item) => acc + item.price, 0);
+
+  const handleRemove = (itemId) => {
+    const updatedCart = cart.filter((item) => item.id !== itemId);
+    setCart(updatedCart);
+  };
+
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-5">Your Cart</h2>
+      <div className="py-15 rounded-xl bg-white p-6  border-2 border-gray-100">
+        {cart.length > 0 ? (
+          <div>
+            {cart.map((item) => (
+              <div
+                key={item.id}
+                className="flex justify-between items-center p-6 bg-gray-50 rounded-lg mb-4 shadow-md"
+              >
+                <div className="flex items-center justify-center gap-5">
+                  <img
+                    src={item.image}
+                    alt={item.title}
+                    className="w-10 h-10 object-contain"
+                  />
+                  <div>
+                    <h3>{item.title}</h3>
+                    <p>${item.price}</p>
+                  </div>
+                </div>
+                <div>
+                  <button
+                    onClick={() => handleRemove(item.id)}
+                    className="btn text-red-500 font-bold text-xl bg-transparent"
+                  >
+                    Remove
+                  </button>
+                </div>
+              </div>
+            ))}
+            <div className="flex justify-between items-center px-6 ">
+              <p>Total</p>
+              <p className="text-xl font-bold">${total.toFixed(2)}</p>
+            </div>
+          </div>
+        ) : (
+          <div className="flex flex-col justify-center items-center gap-6">
+            <ShoppingCart size={50} strokeWidth={2} />
+            <p>Your cart is empty</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
